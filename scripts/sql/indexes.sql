7a8fbc1a9759c577bb7903e997ba137e365c72cc
Holger Bruch <hb@mfdz.de>
2022-03-05 08:54:00 +0100

    New indices

--- scripts/sql/indexes.sql
+++ scripts/sql/indexes.sql
@@ -1,2 +1,4 @@
 CREATE UNIQUE INDEX ON MASTR.MASTR (mastrnummer);
 CREATE INDEX ON MASTR.MASTR (gemeindeschluessel);
+CREATE INDEX ON MASTR.MASTR (InbetriebnahmeDatum);
+CREATE INDEX ON MASTR.MASTR (gemeindeschluessel,InbetriebnahmeDatum,EnergietraegerId);
