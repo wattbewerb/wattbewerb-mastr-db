524c2ad8db4752b4dd0e5278fb7dfb25f808c726
Holger Bruch <hb@mfdz.de>
2024-01-11 12:49:42 +0100

    import municipalities

--- 09_import_bkg.sh
+++ 09_import_bkg.sh
@@ -1,1 +1,2 @@
 shp2pgsql -I -s 25832 -d -D data/bkg/vg250-ew_12-31.utm32s.shape.ebenen/vg250-ew_ebenen_1231/VG250_KRS.shp mastr.vg250_krs | psql -d $1
+shp2pgsql -I -s 25832 -d -D data/bkg/vg250-ew_12-31.utm32s.shape.ebenen/vg250-ew_ebenen_1231/VG250_GEM.shp mastr.vg250_gem | psql -d $1
