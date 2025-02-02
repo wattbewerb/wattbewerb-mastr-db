be16d7303ad3ae6741f055e3ec90354d5906191c
Holger Bruch <hb@mfdz.de>
2025-02-02 09:04:15 +0100

    fix: add refresh for missing mat views

--- scripts/sql/refresh_matviews.sql
+++ scripts/sql/refresh_matviews.sql
@@ -1,7 +1,14 @@
 SET search_path=mastr,public;
 REFRESH MATERIALIZED VIEW statistik_start_per_ags;
+REFRESH MATERIALIZED VIEW statistik_ende_wbw1_per_ags;
+REFRESH MATERIALIZED VIEW statistik_start_per_ags_plausibel;
 REFRESH MATERIALIZED VIEW statistik_heute_per_ags;
 REFRESH MATERIALIZED VIEW nbpruefung_je_netzbetreiber;
 REFRESH MATERIALIZED VIEW checks;
 REFRESH MATERIALIZED VIEW letzte_aktualisierung;
+REFRESH MATERIALIZED VIEW statistik_ende_wbw1_per_ags;
+REFRESH MATERIALIZED VIEW stats_ranking_alle;
 REFRESH MATERIALIZED VIEW pv_groessenklassen_je_ags;
+
+
+
