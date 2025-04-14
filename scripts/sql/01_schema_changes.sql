aad9825008b72b1a47ea1a41a9423c017e41d2b6
Holger Bruch <hb@mfdz.de>
2025-04-14 20:40:11 +0200

    fix: reflect mastr schema changes

---
+++ scripts/sql/01_schema_changes.sql
@@ -0,0 +1,4 @@
+ALTER TABLE mastr.mastr ADD COLUMN ArtDerSolaranlageId INT;
+ALTER TABLE mastr.mastr ADD COLUMN ArtDerSolaranlageBezeichnung TEXT;
+ALTER TABLE mastr.mastr ADD COLUMN WindAnLandOderSeeId INT;
+ALTER TABLE mastr.mastr ADD COLUMN WindAnLandOderSeeBezeichnung TEXT;
